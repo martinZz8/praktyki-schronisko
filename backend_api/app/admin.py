7f67ca707a0fd3509d35f1ecba066cac624ab10e
martinZz8 <57568383+martinZz8@users.noreply.github.com>
2020-07-23 11:50:59 +0200

    up

--- backend_api/app/admin.py
+++ backend_api/app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Admin, Animal, Application, News, Photo
 
 # Register your models here.
+
+admin.site.register(Admin)
+admin.site.register(Animal)
+admin.site.register(Application)
+admin.site.register(News)
+admin.site.register(Photo)
---
+++ backend_api/app/forms.py
@@ -0,0 +1,28 @@
+from .models import Admin, Animal, Application, News, Photo
+from django.forms import ModelForm
+from django import forms
+
+class AdminCreate (forms.ModelForm):
+    class Meta:
+        model = Admin
+        fields = '__all__'
+
+class AnimalCreate (forms.ModelForm):
+    class Meta:
+        model = Animal
+        fields = '__all__'
+
+class ApplicationCreate (forms.ModelForm):
+    class Meta:
+        model = Application
+        fields = '__all__'
+
+class NewsCreate (forms.ModelForm):
+    class Meta:
+        model = News
+        fields = '__all__'
+
+class PhotoCreate (forms.ModelForm):
+    class Meta:
+        model = Photo
+        fields = '__all__'
