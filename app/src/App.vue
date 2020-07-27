8d8bd64ed2008e29d10ca2d1316c63ba0b01ca51
KacperK99 <kkopczacki@gmail.com>
2020-07-27 18:25:32 +0200

    Update App.vue

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,15 +1,14 @@
 <template>
   <div id="app">
     <v-app>
-      <v-app-bar app color="primary" dark>
+      <v-app-bar app color="rgba(57,200,67,1)">
         <div class="d-flex align-center">
           <router-link to="/">
             <v-img :src="require('@/assets/logo.png')" alt="logo" contain height="100" width="50"/>
           </router-link>
         </div>
-        <v-toolbar-title class="ml-5">Schronisko dla zwierząt</v-toolbar-title>
+        <v-toolbar-title class="ml-5"><span style="color: white">Schronisko dla zwierząt</span></v-toolbar-title>
         <!-- Tutaj dodajemy elementy app baru -->
-        <v-btn class="ml-5" text small color="white" to='/'>Strona główna</v-btn>
       </v-app-bar>
       <!-- Nie zmieniać zawartości tagu <v-main> -->
       <v-main><router-view/></v-main>
