3f268d12cebeca2497ee610f998eadc64de363c2
KacperK99 <kkopczacki@gmail.com>
2020-07-27 18:02:55 +0200

    up

--- backend_api/backend_api/settings.py
+++ backend_api/backend_api/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+import posixpath
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -129,3 +131,5 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'backend_api/media')
+
+STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep)+['backend_api']))
--- backend_api/backend_api/urls.py
+++ backend_api/backend_api/urls.py
@@ -22,4 +22,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.image, name='image'),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
