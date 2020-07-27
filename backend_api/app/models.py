bb649b8da3f66ddfa8b702c7e141ea35aca166ed
KacperK99 <kkopczacki@gmail.com>
2020-07-27 13:49:45 +0200

    prace nad zdjeciami

--- backend_api/app/models.py
+++ backend_api/app/models.py
@@ -13,7 +13,7 @@ class Admin(models.Model):
     password = models.CharField(max_length=500)
 
     class Meta:
-        managed = False
+        managed = True
         db_table = 'admin'
 
 
@@ -27,7 +27,7 @@ class Animal(models.Model):
     race = models.CharField(max_length=30)
 
     class Meta:
-        managed = False
+        managed = True
         db_table = 'animal'
 
     def __str__(self):
@@ -44,134 +44,23 @@ class Application(models.Model):
     date = models.DateTimeField()
 
     class Meta:
-        managed = False
+        managed = True
         db_table = 'application'
-
-
-class AuthGroup(models.Model):
-    name = models.CharField(unique=True, max_length=150)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_group'
-
-
-class AuthGroupPermissions(models.Model):
-    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
-    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_group_permissions'
-        unique_together = (('group', 'permission'),)
-
-
-class AuthPermission(models.Model):
-    name = models.CharField(max_length=255)
-    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
-    codename = models.CharField(max_length=100)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_permission'
-        unique_together = (('content_type', 'codename'),)
-
-
-class AuthUser(models.Model):
-    password = models.CharField(max_length=128)
-    last_login = models.DateTimeField(blank=True, null=True)
-    is_superuser = models.BooleanField()
-    username = models.CharField(unique=True, max_length=150)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=150)
-    email = models.CharField(max_length=254)
-    is_staff = models.BooleanField()
-    is_active = models.BooleanField()
-    date_joined = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'auth_user'
-
-
-class AuthUserGroups(models.Model):
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_user_groups'
-        unique_together = (('user', 'group'),)
-
-
-class AuthUserUserPermissions(models.Model):
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_user_user_permissions'
-        unique_together = (('user', 'permission'),)
-
-
-class DjangoAdminLog(models.Model):
-    action_time = models.DateTimeField()
-    object_id = models.TextField(blank=True, null=True)
-    object_repr = models.CharField(max_length=200)
-    action_flag = models.SmallIntegerField()
-    change_message = models.TextField()
-    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'django_admin_log'
-
-
-class DjangoContentType(models.Model):
-    app_label = models.CharField(max_length=100)
-    model = models.CharField(max_length=100)
-
-    class Meta:
-        managed = False
-        db_table = 'django_content_type'
-        unique_together = (('app_label', 'model'),)
-
-
-class DjangoMigrations(models.Model):
-    app = models.CharField(max_length=255)
-    name = models.CharField(max_length=255)
-    applied = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'django_migrations'
-
-
-class DjangoSession(models.Model):
-    session_key = models.CharField(primary_key=True, max_length=40)
-    session_data = models.TextField()
-    expire_date = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'django_session'
-
 
 class News(models.Model):
     content = models.TextField()
     date = models.DateTimeField()
 
     class Meta:
-        managed = False
+        managed = True
         db_table = 'news'
         verbose_name_plural = 'News'
 
 
 class Photo(models.Model):
-    image = models.BinaryField()
+    image = models.ImageField(upload_to = 'image', blank=True)
     animal = models.ForeignKey(Animal, models.DO_NOTHING)
 
     class Meta:
-        managed = False
+        managed = True
         db_table = 'photo'
--- backend_api/app/views.py
+++ backend_api/app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Photo
 
 # Create your views here.
+
+def image(request):
+    image_file = Photo.objects.get(pk=1)
+    return render(request, 'app/test.html', {'zdjecie':image_file})
