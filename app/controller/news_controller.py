310a24ae6a552703c85ede12a5df136b32832e46
KacperK99 <kkopczacki@gmail.com>
2020-08-06 16:25:35 +0200

    up

--- app/controller/news_controller.py
+++ app/controller/news_controller.py
@@ -7,3 +7,7 @@ def get_all_news(request):
 def get_news_by_id(request, id_news):
     news = News.objects.get(ID = id_news)
     return news
+
+def get_last_five_news(request):
+    news=News.objects.all().order_by('-date')[:3]
+    return news
--- app/view/views.py
+++ app/view/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from app.model.models import Photo
 from app.controller.photo_controller import image, images_list, get_thumbnail, get_thumbnails_urls
 from app.controller.animals_controller import get_last_three_animals
+from app.controller.news_controller import get_last_five_news
 
 # Create your views here.
 
@@ -16,4 +17,5 @@ def rendertest2(request):
 def render_home(request):
     last_animals = get_last_three_animals(request)
     last_animals_thumbnails = get_thumbnails_urls(request, last_animals)
-    return render(request, 'app/home.html', {'last_animals':last_animals, 'last_animals_thumbnails':last_animals_thumbnails})
+    last_news = get_last_five_news(request)
+    return render(request, 'app/home.html', {'last_animals':last_animals, 'last_animals_thumbnails':last_animals_thumbnails, 'last_news':last_news})
