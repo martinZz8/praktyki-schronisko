e7b0b4f6167a27a3d2211f635ddc5a7f3cbaaa77
KacperK99 <kkopczacki@gmail.com>
2020-08-11 13:59:34 +0200

    do zmiany inactive i active

--- app/view/views.py
+++ app/view/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from app.model.models import Photo
 from app.controller.photo_controller import image, images_list, get_thumbnail, get_thumbnails_urls
-from app.controller.animals_controller import get_last_three_animals, get_all_animals
+from app.controller.animals_controller import get_last_three_animals, get_all_animals, get_animal_by_id
 from app.controller.news_controller import get_last_five_news, get_all_visible_news, get_news_by_id
 
 # Create your views here.
@@ -25,3 +25,10 @@ def render_animals(request):
     animals = get_all_animals(request)
     animals_thumbnails = get_thumbnails_urls(request, animals)
     return render(request, 'app/animals.html', {'animals':animals, 'animals_thumbnails':animals_thumbnails})
+
+def render_animal(request, id_animal):
+    id_animal = int(id_animal)
+    animal = get_animal_by_id(request, id_animal)
+    thumbnail = get_thumbnail(request, id_animal)
+    photos = images_list(request, id_animal)
+    return render(request, 'app/animal.html', {'animal':animal, 'thumbnail':thumbnail, 'photos':photos})
